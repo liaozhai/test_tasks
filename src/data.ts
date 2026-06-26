98e0885d375b65ac229323fa9a44b11c50f356e5
liaozhai <laocan@yandex.ru>
2026-06-27 00:49:21 +0300

    log

--- src/data.ts
+++ src/data.ts
@@ -3,6 +3,8 @@ import fs from "fs";
 const NUMBER_OF_FEATURES = 1e7;
 
 async function main() {
+    console.log(`Generating ${NUMBER_OF_FEATURES} sample features`);
+    const start = Date.now();
     const w = fs.createWriteStream("data.txt");
     for (let i = 0; i < NUMBER_OF_FEATURES; i++) {
         const x = Math.floor(Math.random() * 180);
@@ -10,6 +12,7 @@ async function main() {
         w.write(`${x};${y}`);
         w.write("\n");
     }
+    console.log(`Done. Time: ${(Date.now() - start) / 1000}s.`);
     w.end();
 }
 
